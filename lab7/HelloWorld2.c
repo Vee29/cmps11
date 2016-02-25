33fa3047e7e46dd1649fdf8a0cd89e7208574d4b
Ben Sherriff <bensherriff@yahoo.com>
2016-02-24 18:15:43 -0800

    Create HelloWorld2.c

---
+++ lab7/HelloWorld2.c
@@ -0,0 +1,11 @@
+/* HelloWorld2.c */
+#include<stdio.h>
+int main(){
+ int a = 6;
+ double x = 2.3;
+ char ch = '$';
+ char hello_string[] = "Hello, World!";
+ printf("a = %d, x = %f, ch = %c\n", a, x, ch);
+ printf("%s%c", hello_string, '\n');
+ return 0;
+}
